c402beb6a6e4242593084796faaf395e46faf883
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2022-06-12 18:57:14 +0000

    Run test

--- templates/typescript/src/main.ts
+++ templates/typescript/src/main.ts
@@ -2,7 +2,11 @@ import { getInputs } from './inputs';
 import { setFailed } from '@actions/core';
 import { run } from './run';
 
-run(getInputs()).catch(err => {
-  const failure = err instanceof Error ? err.message : JSON.stringify(err);
-  setFailed(failure);
-});
+async () => {
+  try {
+    await run(getInputs());
+  } catch (err) {
+    const failure = err instanceof Error ? err.message : JSON.stringify(err);
+    setFailed(failure);
+  }
+};
---
+++ templates/typescript/src/run.test.ts
@@ -0,0 +1,5 @@
+describe('#run', () => {
+  it('is true', () => {
+    expect(true).toBe(true);
+  });
+});
